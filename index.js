40f78faee18abb1318b052d3ff5eff67a5cc3ec4
Samar Omar <samaromm98@gmail.com>
2020-01-21 21:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function writeCards(names,event){
   let a=[];
   for(let i=0; i<names.length;i++){
     let str=names[i].toString();
-    let s='Thank you, '+ str+', for the wonderful '+event +' gift';
+    let s='Thank you, '+ str+', for the wonderful '+event +' gift!';
     a.push(s);
   }
   return a;
