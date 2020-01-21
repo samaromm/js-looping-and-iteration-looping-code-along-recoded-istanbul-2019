1de631a6be82939713b4cd0c3c086711a9786514
Samar Omar <samaromm98@gmail.com>
2020-01-21 21:05:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(names,event){
+  let a=[];
+  for(let i=0; i<names.length;i++){
+    let str=names[i].toString();
+    let s='Thank you, '+ str+', for the wonderful '+event +' gift!';
+    a.push(s);
+  }
+  return a;
+}
+
+function countDown(n){
+  while(n>=0){
+    console.log(n--);
+  }
+}
