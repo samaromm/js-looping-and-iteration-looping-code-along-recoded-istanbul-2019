bea77cf8b0b9cfc03827311fd00e9399f5685e05
Samar Omar <samaromm98@gmail.com>
2020-01-21 20:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(names,event){
+  let a;
+  for(let i=0; i<names.length;i++){
+    let s='Thank you, '+ names[i]+', for the wonderful '+event +' gift';
+    a.push(s);
+  }
+}
