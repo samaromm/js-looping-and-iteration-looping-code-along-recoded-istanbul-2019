b7a879d4e5f12a6957624985160dd0db90fd9515
Samar Omar <samaromm98@gmail.com>
2020-01-21 20:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ writeCards(["Ada", "Brendan", "Ali"], "birthday");
 function writeCards(names,event){
   let a;
   for(let i=0; i<names.length;i++){
-    let s='Thank you, '+ names[i]+', for the wonderful '+event +' gift';
+    let str=names[i].toString();
+    let s='Thank you, '+ str+', for the wonderful '+event +' gift';
     a.push(s);
   }
 }
